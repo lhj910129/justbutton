7e4978adf0a7c25d86cbc1b3abcb62dcc81d295e
이효진 <ihyojin@hlee-MacBookPro.local>
2025-07-28 18:21:54 +0900

    set database user and test

--- src/test/kotlin/com/justbutton/JustbuttonApplicationTests.kt
+++ src/test/kotlin/com/justbutton/JustbuttonApplicationTests.kt
@@ -2,7 +2,9 @@ package com.justbutton
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 
+@ActiveProfiles("test")
 @SpringBootTest
 class JustbuttonApplicationTests {
 
